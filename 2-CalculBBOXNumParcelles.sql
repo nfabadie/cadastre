90b83b81bba124521fcf5d3fa1afae3d09f2c388
Nathalie Abadie <nathalie-f.abadie@ign.fr>
2024-10-16 14:43:39 +0200

    Update 2-CalculBBOXNumParcelles.sql

--- 2-CalculBBOXNumParcelles.sql
+++ 2-CalculBBOXNumParcelles.sql
@@ -3,15 +3,20 @@ CREATE OR REPLACE FUNCTION calculate_parcel_number_bounding_box()
 RETURNS VOID AS $$
 DECLARE
     record RECORD;
-    char_width DOUBLE PRECISION := 2.5; -- Approximation de la largeur moyenne d'un caractère en mètres
-    text_height DOUBLE PRECISION := 3.5; -- Approximation de la hauteur du texte en mètres
+    char_width DOUBLE PRECISION := 3; -- Approximation de la largeur moyenne d'un caractère en mètres
+    text_height DOUBLE PRECISION := 4; -- Approximation de la hauteur du texte en mètres
     text_length INTEGER;
     bbox_width DOUBLE PRECISION;
     bbox_height DOUBLE PRECISION;
 
 BEGIN
+	-- Nettoyage des numéros par suppression des 0 en tête de numéro.
+	ALTER TABLE travail.localisant DROP COLUMN numero_court;
+	ALTER TABLE travail.localisant ADD COLUMN numero_court text;
+	UPDATE travail.localisant SET numero_court = regexp_replace(numero, '(0*)([0-9]*)', '\2');
+	
     -- Boucle sur chaque point pour calculer le rectangle englobant du texte
-    FOR record IN SELECT id, ST_X(geom) AS x, ST_Y(geom) AS y, numero as text FROM travail.localisant LOOP
+    FOR record IN SELECT id, ST_X(geom) AS x, ST_Y(geom) AS y, numero_court as text FROM travail.localisant LOOP
         text_length := LENGTH(record.text);
         bbox_width := char_width * text_length;
         bbox_height := text_height;
